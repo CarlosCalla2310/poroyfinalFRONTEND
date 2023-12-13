9b4b40388b3aa7a0a419ff80bf52a42fbd1c2836
carlos calla <l.illidan1501@gmail.com>
2023-12-12 20:19:26 -0400

    first commit

---
+++ src/screens/MushoOnline/MushoOnline.jsx
@@ -0,0 +1,353 @@
+import React from "react";
+import "./style.css";
+
+
+export const MushoOnline = () => {
+  return (
+    
+    <div className="musho-online">
+      <div className="type-floating">
+        <div className="brand">
+          <img className="img" alt="Icon" src="/img/icon.svg" />
+          <div className="text-wrapper">Esika L&#39;BEL Azzorti</div>
+        </div>
+        <div className="nav-links">
+          <div className="div">
+            <div className="nav-link">
+              <div className="text-wrapper">Inicio</div>
+            </div>
+            <div className="nav-link">
+              <div className="text-wrapper">Nosotros</div>
+            </div>
+            <div className="nav-link">
+              <div className="text-wrapper">Equipo</div>
+            </div>
+            <button className="button">
+              <div className="text-container">
+                <button className="CTA">Ingresar</button>
+              </div>
+            </button>
+            <button className="text-container-wrapper">
+              <div className="text-container">
+                <button className="CTA">Registrarse</button>
+
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="type-vertical">
+        <div className="image-container">
+          <img className="image-dalle" alt="Image dalle" src="/img/image-dalle.png" />
+        </div>
+        <div className="copy-component">
+          <div className="heading-text">
+            <p className="heading">Bienvenido a tu tienda de bellezasfefrwerew</p>
+            <p className="subheading">Explora nuestra gama exclusiva de productos.</p>
+          </div>
+          <div className="CTA-button">
+            <div className="text-container">
+              <button className="CTA-2">Descubre Más</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="type-super-grid">
+        <div className="content">
+          <div className="heading-container">
+            <div className="heading-2">Destacados de la Temporada</div>
+            <div className="text-wrapper-2">Nuevas tendencias en belleza.</div>
+          </div>
+          <div className="showcase-grid">
+            <div className="cards">
+              <img className="image-aisplash" alt="Image aisplash" src="/img/image-aisplash.png" />
+              <div className="copy">
+                <div className="feature-title">Esika</div>
+              </div>
+            </div>
+            <div className="cards">
+              <img className="image-aisplash" alt="Image aisplash" src="/img/image-aisplash-1.png" />
+              <div className="copy">
+                <div className="feature-title">L&#39;BEL</div>
+              </div>
+            </div>
+            <div className="cards">
+              <img className="image-aisplash" alt="Image aisplash" src="/img/image-aisplash-2.png" />
+              <div className="copy">
+                <div className="feature-title">Azzorti</div>
+              </div>
+            </div>
+            <div className="cards">
+              <img className="image-aisplash" alt="Image aisplash" src="/img/image-aisplash-3.png" />
+              <div className="copy">
+                <div className="feature-title">Nuevas Llegadas</div>
+              </div>
+            </div>
+            <div className="cards">
+              <img className="image-aisplash" alt="Image aisplash" src="/img/image-aisplash-4.png" />
+              <div className="copy">
+                <div className="feature-title">Más Vendidos</div>
+              </div>
+            </div>
+            <div className="cards">
+              <img className="image-aisplash" alt="Image aisplash" src="/img/image-aisplash-5.png" />
+              <div className="copy">
+                <div className="feature-title">Ofertas Especiales</div>
+              </div>
+            </div>
+            <div className="cards">
+              <img className="image-aisplash" alt="Image aisplash" src="/img/image-aisplash-6.png" />
+              <div className="copy">
+                <div className="feature-title">Productos Destacados</div>
+              </div>
+            </div>
+            <div className="cards">
+              <img className="image-aisplash" alt="Image aisplash" src="/img/image-aisplash-7.png" />
+              <div className="copy">
+                <div className="feature-title">Edición Limitada</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="type-mobile-download">
+        <div className="container">
+          <div className="copy-component-2">
+            <div className="heading-text-2">
+              <div className="heading-3">Descubre tu belleza interior</div>
+              <p className="p">Descarga nuestra aplicación para una experiencia de compra superior.</p>
+            </div>
+            <div className="app-download-combo">
+              <div className="app-store">
+                <img className="apple-icon" alt="Apple icon" src="/img/apple-icon.svg" />
+                <img className="app-store-2" alt="App store" src="/img/app-store.svg" />
+                <img className="download" alt="Download" src="/img/download.svg" />
+              </div>
+              <img className="google-play" alt="Google play" src="/img/google-play.svg" />
+            </div>
+          </div>
+          <div className="visual-comp">
+            <div className="device">
+              <div className="overlap-group">
+                <img className="screen-content" alt="Screen content" src="/img/screen-content-aisplash.png" />
+              </div>
+            </div>
+            <div className="background-image" />
+          </div>
+        </div>
+      </div>
+      <div className="property-highlight">
+        <div className="heading-text-wrapper">
+          <div className="heading-text-2">
+            <div className="heading-4">Precios</div>
+            <p className="subheading-2">Ofrecemos los mejores precios para productos de alta calidad.</p>
+          </div>
+        </div>
+        <div className="price-plans">
+          <div className="plan">
+            <div className="heading-5">
+              <img className="img-2" alt="Icon icon" src="/img/icon-icon.svg" />
+              <div className="title">Básico</div>
+              <div className="short-description">Ideal para compradores individuales</div>
+            </div>
+            <div className="div-2">
+              <div className="element">$10</div>
+              <div className="details-container">
+                <div className="text-wrapper-3">por mes</div>
+                <div className="yearly-switch">
+                  <div className="text-wrapper-3">anual</div>
+                  <div className="switch">
+                    <div className="circle" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="div-3">
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Artículo de Lista</div>
+              </div>
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">5 GB de Almacenamiento</div>
+              </div>
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Soporte Básico</div>
+              </div>
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Acceso a la Comunidad</div>
+              </div>
+            </div>
+            <button className="div-wrapper">
+              <div className="text-container">
+                <button className="CTA-3">Descubre Más</button>
+              </div>
+            </button>
+          </div>
+          <div className="plan">
+            <div className="heading-5">
+              <img className="img-2" alt="Icon icon" src="/img/icon-icon-1.svg" />
+              <div className="title">Pro</div>
+              <p className="short-description">Elección popular para pequeñas empresas</p>
+            </div>
+            <div className="div-2">
+              <div className="price">
+                <div className="element">$15</div>
+                <div className="element-2">100</div>
+              </div>
+              <div className="details-container">
+                <div className="text-wrapper-3">por mes</div>
+                <div className="yearly-switch">
+                  <div className="text-wrapper-3">anual</div>
+                  <div className="switch">
+                    <div className="circle" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="div-3">
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Proyectos Ilimitados</div>
+              </div>
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Almacenamiento Ilimitado</div>
+              </div>
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Soporte Dedicado 24/7</div>
+              </div>
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Integraciones Personalizadas</div>
+              </div>
+            </div>
+            <button className="button-2">
+              <div className="text-container">
+                <button className="CTA-3">Descubre Más</button>
+              </div>
+            </button>
+          </div>
+          <div className="plan">
+            <div className="heading-5">
+              <img className="img-2" alt="Icon" src="/img/icon-1.svg" />
+              <div className="title">Empresarial</div>
+              <div className="short-description">Ideal para grandes empresas</div>
+            </div>
+            <div className="div-2">
+              <div className="element">$20</div>
+              <div className="details-container">
+                <div className="text-wrapper-3">por mes</div>
+                <div className="yearly-switch">
+                  <div className="text-wrapper-3">anual</div>
+                  <div className="switch">
+                    <div className="circle" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="div-3">
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Artículo de Lista</div>
+              </div>
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">5 GB de Almacenamiento</div>
+              </div>
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Soporte Básico</div>
+              </div>
+              <div className="div-2">
+                <img className="img" alt="Checkcircle icon" src="/img/checkcircle-icon-11.svg" />
+                <div className="list-item">Acceso a la Comunidad</div>
+              </div>
+            </div>
+            <button className="div-wrapper">
+              <div className="text-container">
+                <button className="CTA-3">Descubre Más</button>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="property-steps">
+        <div className="heading-wrapper">
+          <div className="heading-container-2">
+            <img className="image-aisplash-2" alt="Image aisplash" src="/img/image-aisplash-8.png" />
+            <div className="heading-text-wrapper">
+              <div className="heading-text-2">
+                <div className="heading-6">Cómo Funciona</div>
+                <p className="subheading-3">Descubre cómo comprar tus productos favoritos.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="steps-wrapper">
+          <div className="how-it-works-grid">
+            <div className="step">
+              <div className="step-title">Escoge</div>
+              <div className="step-description">Selecciona tus productos favoritos.</div>
+            </div>
+            <div className="step">
+              <div className="step-title">Compra</div>
+              <p className="step-description">Realiza tu compra de manera segura.</p>
+            </div>
+            <div className="step">
+              <div className="step-title">Disfruta</div>
+              <p className="step-description">Recibe y disfruta de tus productos.</p>
+            </div>
+            <div className="step">
+              <div className="step-title">Repite</div>
+              <p className="step-description">Repite el proceso cuando quieras.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="type-with-newsletter">
+        <div className="content-2">
+          <div className="list">
+            <div className="list-heading">Empresa</div>
+            <div className="list-link-item">Sobre Nosotros</div>
+            <div className="list-link-item">Carreras</div>
+            <div className="list-link-item">Sala de Prensa</div>
+          </div>
+          <div className="list">
+            <div className="list-heading">Características</div>
+            <div className="list-link-item">Rapidez</div>
+            <div className="list-link-item">Tarjetas</div>
+            <div className="list-link-item">Noticias</div>
+          </div>
+          <div className="list">
+            <div className="list-heading">Social</div>
+            <div className="list-link-item">Twitter</div>
+            <div className="list-link-item">Instagram</div>
+            <div className="list-link-item">Hilos</div>
+          </div>
+          <div className="list-2">
+            <div className="div-3">
+              <div className="list-heading-2">Boletín Mensual</div>
+              <p className="text-wrapper-2">Recibe consejos profesionales para tus rutinas de belleza</p>
+            </div>
+            <div className="input-with-button">
+              <div className="input">
+                <div className="input-form">
+                  <input className="placeholder-text" placeholder="tunombre@ejemplo.com" type="email" />
+                </div>
+              </div>
+              <img className="button-3" alt="Button" src="/img/button.svg" />
+            </div>
+            <div className="links">
+              <div className="list-link-item-2">Términos</div>
+              <div className="list-link-item-2">Privacidad</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/screens/MushoOnline/index.js
@@ -0,0 +1,1 @@
+export { MushoOnline } from "./MushoOnline";
